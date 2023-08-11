a1c460931356df62ff74f68b5d2f7df465a164e8
KylymZ <zenisbekovk04@gmail.com>
2023-08-12 01:32:14 +0600

    first commit

---
+++ GoodsStore/GoodsStore/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodsStore.Models
+{
+    public class Products
+    {
+        public int id { get; set; }
+        public string name { get; set; }=string.Empty;
+        public double price { get; set; }
+        public string image_url { get; set; } = string.Empty;
+    }
+}
